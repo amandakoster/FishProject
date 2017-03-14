16f4b74e271c5db96eae2ec804c9cc6b1cbdf2df
Amanda Koster <amandakoster@gmail.com>
2017-03-13 18:32:31 -0700

    Mmore new

--- script.js
+++ script.js
@@ -3,13 +3,24 @@
 // Display the values of each array as unordered lists in the browser
 // Calculating the sum of these hourly totals; your output for each location should look like this:
 
-var storeHours = ['6a', '7a', '8a', '9a', '10a', '11a', '12a', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', 'total'];
+var storeHours = ['6a', '7a', '8a', '9a', '10a', '11a', '12a', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a'];
 
 var firstPike = {
   name: '1st and Pike',
   minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
+  renderHTML: function() {
+    this.totalSales();
+    var newUl = document.createElement('ul');
+    for (var i = 0; i <= this.salesArray.length; i++) {
+      var newLi = document.createElement('li');
+      newLi.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
+      newUl.appendChild(newLi);
+    }
+    var body = document.getElementById('body');
+    body.appendChild(newUl);
+  },
   randomCust: function() {
     return Math.floor(Math.random() * ((this.maxCust - this.minCust + 1) + this.minCust));
   },
@@ -21,5 +32,15 @@ var firstPike = {
     for (var i = 0; i < storeHours.length; i++) {
       salesArray.push(this.hourlySales());
     }
+    this.salesArray = salesArray;
+    return salesArray;
   }
 };
+firstPike.renderHTML();
+
+// /var firstItem = document.getElementById('body');
+// var navList = document.getElementsByTagName('ul')[0];
+// var newListItem = document.createElement('li');
+// newListItem.appendChild(newLink);
+// newListItem.setAttribute('class', 'some-list-class');
+// navList.appendChild(newListItem);
