589c338ed440ce1e4238b0edc1da9b6923e90df8
Amanda Koster <amandakoster@gmail.com>
2017-03-15 16:50:53 -0700

    Fish-table-fixed

--- script.js
+++ script.js
@@ -1,234 +1,137 @@
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-///FirstPike
-var firstPike = {
-  name: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCookieSale: 6.3,
-  total: 0,
-  salesArray: [],
-  renderHTML: function() {
-    this.totalSales();
-    var newUl = document.createElement('ul');
+var firstPike = new Store('1st and Pike', 23, 65, 6.3);
+var seaTac = new Store('SeaTac Airport', 3, 24, 1.3);
+var seaCenter = new Store('Seattle Center', 11, 38, 2.3);
+var capHill = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+
+function Store(name, minCust, maxCust, avgCookieSale) {
+
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSale = avgCookieSale;
+  this.total = 0;
+  this.salesArray = [];
+  var salesArray = this.salesArray;
+  var total = this.total;
+
+  this.randomCust = function() {
+    return Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
+  };
+
+  this.randomHourlySale = function() {
+    return Math.floor(this.randomCust() * avgCookieSale);
+  };
+
+  this.totalSales = function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      var randomHourlySale = this.randomHourlySale();
+      salesArray.push(randomHourlySale);
+      this.total += randomHourlySale;
+    }
+  };
+
+  this.totalSales();
+  this.renderHTML = function() {
+
     var newUl = document.createElement('ul');
     var newHeading = document.createElement('h2');
-    newHeading.innerText = this.name;
+    newHeading.innerText = name;
     body.appendChild(newHeading);
-    for (var i = 0; i < this.salesArray.length; i++) {
+
+    for (var i = 0; i < salesArray.length; i++) {
       var newLi = document.createElement('li');
-      newLi.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
+      newLi.innerText = storeHours[i] + ': ' + salesArray[i] + ' cookies';
       newUl.appendChild(newLi);
     }
+
     var TotalLi = document.createElement('li');
     TotalLi.innerText = 'Total' + ': ' + this.total + ' cookies';
     newUl.appendChild(TotalLi);
     body.appendChild(newUl);
-  },
-  randomCust: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-  hourlySales: function() {
-    return Math.floor(this.randomCust() * this.avgCookieSale);
-  },
-  totalSales: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var hourlySales = this.hourlySales();
-      this.salesArray.push(hourlySales);
-      this.total += hourlySales;
-    }
-  }
+  };
 };
-firstPike.renderHTML();
 
-//Form
-var form = document.getElementById('the-form');
-body.appendChild(form);
-var btn = document.getElementById('submit'); btn.disabled = false;
-body.appendChild(btn);
+//Table
+//table function
+tabFuction = function() {
+  var table = document.createElement('table');
+  body.appendChild(table);
 
-var theFormItself = event.target;
-  // the "elements" attribute of the event.target object holds (for a form) all of the form fields by name
-  console.log(theFormItself.elements['name', 'minCust', 'maxCust', 'avgCookieSale'].value);
+// table head
+  var tHead = document.createElement('thead');
+  table.appendChild(tHead);
+  var headRow = document.createElement('tr');
+  tHead.appendChild(headRow);
+
+  var spacer = document.createElement('th');
+  headRow.appendChild(spacer);
+
+  for (var i = 0; i < storeHours.length; i++) {
+    var tData = document.createElement('th');
+    tData.innerText = storeHours[i];
+    headRow.appendChild(tData);
   };
 
-form.addEventListener('submit', alertTheUser);
+  //table body
+  var tBody = document.createElement('tbody');
+  table.appendChild(tBody);
 
+  var tabRow = document.createElement('tr');
+  tBody.appendChild(tabRow);
 
-function alertTheUser(event){
-  event.preventDefault(); // stops the form from submitting and leaving the page.
-  // time for the harvest
-  var theFormItself = event.target;
-  // the "elements" attribute of the event.target object holds (for a form) all of the form fields by name
-  console.log(theFormItself.elements['firstname'].value);
+  var rowName = document.createElement('th');
+  tabRow.appendChild(rowName);
+  rowName.innerText = firstPike.name;
+
+  for (var i = 0; i < storeHours.length; i++) {
+    var tData = document.createElement('td');
+    tData.innerText = firstPike.salesArray[i];
+    tabRow.appendChild(tData);
+  };
+
+  function newTableRow(store) {
+    var tabRow = document.createElement('tr');
+    tBody.appendChild(tabRow);
+
+    var rowName = document.createElement('th');
+    tabRow.appendChild(rowName);
+    rowName.innerText = store.name;
+
+    for (var i = 0; i < storeHours.length; i++) {
+      var tData = document.createElement('td');
+      tData.innerText = store.salesArray[i];
+      tabRow.appendChild(tData);
+    };
+  }
+
+  alertTheUser = function(event){
+    var form = document.getElementById('the-form');
+    body.appendChild(form);
+    var formName = document.getElementById('submit');
+    body.appendChild(btn);
+    var fMin = document.getElementById('minCust');
+    body.appendChild(form);
+    var fromMax = document.getElementById('maxCust');
+    body.appendChild(form);
+    var bform = document.getElementById('submit'); btn.disabled = true;
+    body.appendChild(btn);
+    event.preventDefault();
+  };
+
+  newTableRow(firstPike);
+  newTableRow(seaTac);
+  newTableRow(seaCenter);
+  newTableRow(capHill);
+  newTableRow(alki);
+
+//footer
+  var tFoot = document.createElement('tf');
+  body.appendChild(tFoot);
 };
-form.addEventListener('submit', alertTheUser);
 
+///End Table
 
-//
-// ///SeaTac Airport
-// var seaTac = {
-//   name: 'SeaTac Airport',
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookieSale: 1.2,
-//   total: 0,
-//   salesArray: [],
-//   renderHTML: function() {
-//     this.totalSales();
-//     var newUl = document.createElement('ul');
-//     var newUl = document.createElement('ul');
-//     var newHeading = document.createElement('h2');
-//     newHeading.innerText = this.name;
-//     body.appendChild(newHeading);
-//     for (var i = 0; i < this.salesArray.length; i++) {
-//       var newLi = document.createElement('li');
-//       newLi.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-//       newUl.appendChild(newLi);
-//     }
-//     var TotalLi = document.createElement('li');
-//     TotalLi.innerText = 'Total' + ': ' + this.total + ' cookies';
-//     newUl.appendChild(TotalLi);
-//     body.appendChild(newUl);
-//   },
-//   randomCust: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-//   },
-//   hourlySales: function() {
-//     return Math.floor(this.randomCust() * this.avgCookieSale);
-//   },
-//   totalSales: function() {
-//     for (var i = 0; i < storeHours.length; i++) {
-//       var hourlySales = this.hourlySales();
-//       this.salesArray.push(hourlySales);
-//       this.total += hourlySales;
-//     }
-//   }
-// };
-// seaTac.renderHTML();
-//
-// ///Seattle Center
-// var seaCenter = {
-//   name: 'Seattle Center',
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCookieSale: 3.7,
-//   total: 0,
-//   salesArray: [],
-//   renderHTML: function() {
-//     this.totalSales();
-//     // this.total = this.totalSales() * this.salesArray[i];
-//     var newUl = document.createElement('ul');
-//     var newUl = document.createElement('ul');
-//     var newHeading = document.createElement('h2');
-//     newHeading.innerText = this.name;
-//     body.appendChild(newHeading);
-//     for (var i = 0; i < this.salesArray.length; i++) {
-//       var newLi = document.createElement('li');
-//       newLi.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-//       newUl.appendChild(newLi);
-//     }
-//     var TotalLi = document.createElement('li');
-//     TotalLi.innerText = 'Total' + ': ' + this.total + ' cookies';
-//     newUl.appendChild(TotalLi);
-//
-//     body.appendChild(newUl);
-//   },
-//   randomCust: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-//   },
-//   hourlySales: function() {
-//     return Math.floor(this.randomCust() * this.avgCookieSale);
-//   },
-//   totalSales: function() {
-//     for (var i = 0; i < storeHours.length; i++) {
-//       var hourlySales = this.hourlySales();
-//       this.salesArray.push(hourlySales);
-//       this.total += hourlySales;
-//     }
-//   }
-// };
-// seaCenter.renderHTML();
-//
-// ///Capitol Hill
-// var capHill = {
-//   name: 'Capitol Hill',
-//   minCust: 20,
-//   maxCust: 38,
-//   avgCookieSale: 2.3,
-//   total: 0,
-//   salesArray: [],
-//   renderHTML: function() {
-//     this.totalSales();
-//     // this.total = this.totalSales() * this.salesArray[i];
-//     var newUl = document.createElement('ul');
-//     var newUl = document.createElement('ul');
-//     var newHeading = document.createElement('h2');
-//     newHeading.innerText = this.name;
-//     body.appendChild(newHeading);
-//     for (var i = 0; i < this.salesArray.length; i++) {
-//       var newLi = document.createElement('li');
-//       newLi.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-//       newUl.appendChild(newLi);
-//     }
-//     var TotalLi = document.createElement('li');
-//     TotalLi.innerText = 'Total' + ': ' + this.total + ' cookies';
-//     newUl.appendChild(TotalLi);
-//     body.appendChild(newUl);
-//   },
-//   randomCust: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-//   },
-//   hourlySales: function() {
-//     return Math.floor(this.randomCust() * this.avgCookieSale);
-//   },
-//   totalSales: function() {
-//     for (var i = 0; i < storeHours.length; i++) {
-//       var hourlySales = this.hourlySales();
-//       this.salesArray.push(hourlySales);
-//       this.total += hourlySales;
-//     }
-//   }
-// };
-// capHill.renderHTML();
-//
-// ///Alki
-// var Alki = {
-//   name: 'Alki',
-//   minCust: 2,
-//   maxCust: 16,
-//   avgCookieSale: 4.,
-//   total: 0,
-//   salesArray: [],
-//   renderHTML: function() {
-//     this.totalSales();
-//     var newUl = document.createElement('ul');
-//     var newUl = document.createElement('ul');
-//     var newHeading = document.createElement('h2');
-//     newHeading.innerText = this.name;
-//     body.appendChild(newHeading);
-//     for (var i = 0; i < this.salesArray.length; i++) {
-//       var newLi = document.createElement('li');
-//       newLi.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-//       newUl.appendChild(newLi);
-//     }
-//     var TotalLi = document.createElement('li');
-//     TotalLi.innerText = 'Total' + ': ' + this.total + ' cookies';
-//     newUl.appendChild(TotalLi);
-//     body.appendChild(newUl);
-//   },
-//   randomCust: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-//   },
-//   hourlySales: function() {
-//     return Math.floor(this.randomCust() * this.avgCookieSale);
-//   },
-//   totalSales: function() {
-//     for (var i = 0; i < storeHours.length; i++) {
-//       var hourlySales = this.hourlySales();
-//       this.salesArray.push(hourlySales);
-//       this.total += hourlySales;
-//     }
-//   }
-// };
-// Alki.renderHTML();
+tabFuction();
--- test.js
+++ test.js
@@ -1,115 +1,71 @@
+
+
+// var form = document.getElementById('the-form');
+//
+// function addNewStore(event) {
+//   event.preventDefault();
+//   var newName = event.target.storeName.value;
+//   console.log(newName);
+// }
+
+
+// function(event){
+//   var form = document.getElementById('the-form');
+//   var formName = document.getElementById('submit');
+//   var fMin = document.getElementById('minCust');
+//   var fromMax = document.getElementById('maxCust');
+//   var formButton = document.getElementById('submit'); btn.disabled = true;
+//   event.preventDefault();
+// };
+
+
 'use strict';
 
-var shop= {
-  wheels: 4,
-  isMoving: false,
-  'year made': 2016,
-  manufacturer: 'Nissan',
-  drive: function(){
-    var foo = 'bar';
-    this.isMoving = true;
-    if (this.isMoving) {
-      this.lockDoors();
-    }
-    console.log('Is this car moving? ' + this.isMoving);
-  },
-  lockDoors: function(){
-    this.doorsLocked = true;
-  },
-  brake: function(){
-    this.isMoving = false;
-  }
-};
-car.isMoving = 'banana';
-car['isMoving'] = [1, 2, 3];
+alertTheUser = function(event){
+  var form = document.getElementById('the-form');
+  body.appendChild(form);
+  var btn = document.getElementById('submit');
+  body.appendChild(btn);
+  var form = document.getElementById('minCust');
+  body.appendChild(form);
+  var btn = document.getElementById('maxCust');
+  body.appendChild(form);
+  var btn = document.getElementById('submit'); btn.disabled = true;
+  body.appendChild(btn);
+  event.preventDefault();
+} // stops the form from submitting and leaving the page.
+  // time for the harvest
+  var theFormItself = event.target;
+  // the "elements" attribute of the event.target object holds (for a form) all of the form fields by name
+  console.log(theFormItself.elements['firstname'].value);
+}
 
-function Building(name, value, address) {
-  this.buildingName = name;
-  this.evaluatedAmount = value;
-  this.address = address;
-  var bathroom = 'operational';
-  this.inhabited = function(){
-    this.tenants = true;
+
+function capitalizeEverything(event){
+  // var theText = this.value;
+  this.value = "You've focused on me!";
+  console.log(this);
+  console.log(event.target);
+}
+firstname.addEventListener('focus', capitalizeEverything);
+
+//Form
+
+var theFormItself = event.target;
+  the "elements" attribute of the event.target object holds (for a form) all of the form fields by name
+  console.log(theFormItself.elements['name', 'minCust', 'maxCust', 'avgCookieSale'].value);
   };
-}
 
-function Person(name, age, grade){
-  this.name = name;
-  this.age = age;
-  this.grade = grade;
-  var divisor = 25;
-  this.calculateGPA = function(){
-    return this.grade / divisor;
-  };
-}
-var david = new Person('David', '30-something', 92);
-var amanda = new Person('Amanda', null, 97);
-var people = [];
+form.addEventListener('submit', alertTheUser);
 
-var names = ['David', 'Amanda', 'Becky', 'Ben'];
-var ages = [88, 5, 102, -12];
-for (var i = 0; i < names.length; i++) {
-  people.push(new Person(names[i], ages[i], Math.random()));
-  console.log('Just made ' + names[i]);
-}
+target.addEventListener(type, listener[, options]);
+target.addEventListener(type, listener[, useCapture]);
 
-var newNav = ['Facebook', 'Instagram', 'Twitter'];
-
-var navLinks = document.getElementsByClassName('nav-link');
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].innerText = newNav[i];
-}
-
-var user = {
-  authenticated: true
+function alertTheUser(event){
+  event.preventDefault(); // stops the form from submitting and leaving the page.
+  // time for the harvest
+  var theFormItself = event.target;
+  // the "elements" attribute of the event.target object holds (for a form) all of the form fields by name
+  console.log(theFormItself.elements['firstname'].value);
 };
-
-var navList = document.getElementsByTagName('ul')[0];
-var newListItem = document.createElement('li');
-var newLink = document.createElement('a');
-newLink.setAttribute('href', 'http://news.ycombinator.com');
-newLink.innerText = 'Hacker News';
-newListItem.appendChild(newLink);
-newListItem.setAttribute('class', 'some-list-class');
-navList.appendChild(newListItem);
-
-var hours = ['8am', '9am', ...]; // this is not functioning js
-
-function () {
-  for (var i = 0; i < hours.length; i++) {
-    // do some operation
-    // this operation should push new cookie sales for every hour
-  }
-}
-
-var location = SeaCenter {
-  name: 'Seattle Center',
-  minCust: 11,
-  maxCust: 38,
-  avgCookieSale: 3.7,
-  hours : [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8],
-};
-
-var location = capHill {
-  name: 'Capitol Hill	',
-  minCust: 20,
-  maxCust: 38,
-  avgCookieSale: 2.3,
-  hours : [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8],
-};
-
-var location = Alki {
-  name: 'Alki',
-  minCust: 2,
-  maxCust: 16,
-  avgCookieSale: 4.6,
-  hours : [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8],
-};
-
-var location = SeaTac {
-  name: 'SeaTac Airport',
-  minCust: 3,
-  maxCust: 24,
-  avgCookieSale: 1.2,
-  hours : [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8],
-};
+form.addEventListener('submit', alertTheUser);
