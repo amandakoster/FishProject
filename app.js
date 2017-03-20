83df0e410898b529d3ce59b6a9beaa3bbacd5e1f
Amanda Koster <amandakoster@gmail.com>
2017-03-19 18:24:05 -0700

    aded total table

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-'use strict';
 
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var storeArray = [];
 var firstPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.3);
 var seaCenter = new Store('Seattle Center', 11, 38, 2.3);
 var capHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
-//store object:
 function Store(name, minCust, maxCust, avgCookieSale) {
   this.name = name;
   this.minCust = minCust;
@@ -15,7 +14,6 @@ function Store(name, minCust, maxCust, avgCookieSale) {
   this.avgCookieSale = avgCookieSale;
   this.total = 0;
   this.salesArray = [];
-  var salesArray = this.salesArray;
   var total = this.total;
 
   this.randomCust = function() {
@@ -26,53 +24,19 @@ function Store(name, minCust, maxCust, avgCookieSale) {
     return Math.floor(this.randomCust() * avgCookieSale);
   };
 
-  this.cookiesPuchPerHour = function() {
+  this.totalSales = function() {
     for (var i = 0; i < storeHours.length; i++) {
       var randomHourlySale = this.randomHourlySale();
-      salesArray.push(randomHourlySale);
+      this.salesArray.push(randomHourlySale);
       this.total += randomHourlySale;
     }
   };
 
-  this.cookiesPuchPerHour();
-  this.renderHTML = function() {
-
-    var newUl = document.createElement('ul');
-    var newHeading = document.createElement('h2');
-    newHeading.innerText = name;
-    body.appendChild(newHeading);
-
-    for (var i = 0; i < salesArray.length; i++) {
-      var newLi = document.createElement('li');
-      newLi.innerText = storeHours[i] + ': ' + salesArray[i] + ' cookies';
-      newUl.appendChild(newLi);
-    }
-
-    var TotalLi = document.createElement('li');
-    TotalLi.innerText = 'Total' + ': ' + this.total + ' cookies';
-    newUl.appendChild(TotalLi);
-    body.appendChild(newUl);
-  };
+  this.totalSales();
+  storeArray.push(this);
 };
+console.log(storeArray);
 
-function createNewStore(event) {
-  event.preventDefault();
-  var name = event.target.elements.storeName;
-  var minCust = event.target.elements.minCust;
-  var maxCust = event.target.elements.maxCust;
-  var avgCookieSale = event.target.elements.aveCookies;
-
-  if (maxCust < minCust) {
-    alert('The max number of customers should not be larger than the min number of customers');
-  } else {
-    var newStore = new Store(name.value, Math.floor(minCust.value), Math.floor(maxCust.value), avgCookieSale.value);
-    newTableRow(newStore);
-
-    form.reset();
-  }
-}
-
-//Table
 //table function
 var tabFunction = function() {
   var table = document.createElement('table');
@@ -97,6 +61,10 @@ var tabFunction = function() {
   var tBody = document.createElement('tbody');
   table.appendChild(tBody);
 
+  var tFoot = document.createElement('tfoot');
+  tFoot.id = 'bob';
+  table.appendChild(tFoot);
+
   var tabRow = document.createElement('tr');
   tBody.appendChild(tabRow);
 
@@ -111,6 +79,7 @@ function newTableRow(store) {
   var tBody = document.getElementsByTagName('tbody')[0];
   var tabRow = document.createElement('tr');
   tBody.appendChild(tabRow);
+
   var rowName = document.createElement('th');
   tabRow.appendChild(rowName);
   rowName.innerText = store.name;
@@ -121,71 +90,53 @@ function newTableRow(store) {
     tabRow.appendChild(tData);
   };
 }
+///End Table
 tabFunction();
 
-// form start
+//form
 var form = document.getElementById('the-form');
-
-// form End
-
-function hourlyStoresTotal() {
-  var table = document.getElementById('bodyTable');
-  var tfoot = document.getElementById('tableFoot');
-  table.appendChild(tfoot);
-  var tr = document.createElement('tr');
-  tfoot.appendChild(tr);
-  tr.id = 'totalsRow';
-  var th = document.createElement('th');
-  th.innerText = 'Total';
-  tr.appendChild(th);
-  for (var i = 0; i < hours.length - 2; i++) {
-    var hrlyTotal = 0;
-    for (var x = 0; x < allStores.length; x++) {
-      hrlyTotal += allStores[x].salesArray[i];
-    }
-    var footTD = document.createElement('td');
-    footTD.innerText = hrlyTotal;
-    footTD.style.backgroundColor = '#40B27C';
-    tr.appendChild(footTD);
-  }
-  // For adding that last cell of the total of totals
-  var totalTotal = 0;
-  for (var i = 0; i < allStores.length; i++) {
-    totalTotal += allStores[i].total;
-  }
-  var totalTd = document.createElement('td');
-  totalTd.innerText = totalTotal;
-  totalTd.style.backgroundColor = '#40B27C';
-  tr.appendChild(totalTd);
-};
-// hourlyStoresTotal(); // this was in the wrong place
-
-for (var i = 0; i < allStores.length; i++) {
-  allStores[i].generateTableRow();
-}
-hourlyStoresTotal(); // moved it down here, so that it executes AFTER sales are generated above.
-
-var elStoreForm = document.getElementById('newStoreForm');
-
-function submitButton(event) {
+function createNewStore(event) {
   event.preventDefault();
-  var newStoreForm = event.target;
-  var strLoc = event.target.storeLocation;
-  var minCst = event.target.minimumCustomers;
-  var maxCst = event.target.maximumCustomers;
-  var avgCook = event.target.averageCookies;
-  if (minCst > maxCst) {
-    alert('Please double check the number values for accuracy and re-submit the form. Thank you.');
+  var name = event.target.elements.storeName;
+  var minCust = event.target.elements.minCust;
+  var maxCust = event.target.elements.maxCust;
+  var avgCookieSale = event.target.elements.aveCookies;
+
+  if (maxCust < minCust) {
+    alert('The max number of customers should not be larger than the min number of customers');
   } else {
-    var formVariables = new CreateStore(strLoc.value, minCst.value, maxCst.value, avgCook.value);
-    formVariables.generateTableRow();
-    // making sure we add appropriate totals
-    var tfoot = document.getElementById('tableFoot');
-    var totalsRow = document.getElementById('totalsRow');
-    tfoot.removeChild(totalsRow); // first remove the existing totals row
-    hourlyStoresTotal(); // then append a new, updated totals row
+    var newStore = new Store(name.value, Math.floor(minCust.value), Math.floor(maxCust.value), avgCookieSale.value);
+    newTableRow(newStore);
+    var tFoot = document.getElementById('bob');
+    var totalsRow = document.getElementById('shirley');
+    tFoot.removeChild(totalsRow);
+    hourlyTotals();
+    form.reset();
   }
-  elStoreForm.reset();
 }
 
-elStoreForm.addEventListener('submit',submitButton);
+//hourly totals
+function hourlyTotals(){
+  var tFoot = document.getElementsByTagName('tfoot')[0];
+  var tabRow = document.createElement('tr');
+  tabRow.id = 'shirley';
+  tFoot.appendChild(tabRow);
+  var rowName = document.createElement('th');
+  tabRow.appendChild(rowName);
+  rowName.innerText = 'Total Sales';
+
+  for (h = 0; h < storeHours.length; h++) {
+    var tData = document.createElement('td');
+    var hourly = 0;
+    for (j = 0; j < storeArray.length; j++){
+      hourly += storeArray[j].salesArray[h];
+
+    }
+    console.log(tData);
+    tData.innerText = hourly;
+    tabRow.appendChild(tData);
+  }
+};
+
+hourlyTotals();
+form.addEventListener('submit', createNewStore);
